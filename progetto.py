0f42c6c78bd2e3ecd87c5eb6f73b74cdff2484cb
Matteo Brina <matteobrina99@gmail.com>
2023-08-17 16:31:05 +0200

    modificato progetto.py

--- progetto.py
+++ progetto.py
@@ -1,5 +1,9 @@
 import nodo
 import node_generator as ng
+import cluster as cl
+import utili as ut
+import route as rt
+
 
 lista= ng.genera_lista()
 for el in lista:
@@ -10,4 +14,20 @@ with open('nodi.txt', 'w') as f:
         f.write(str(el)+ '\n')
 
 
+nodo1 = nodo.Nodo(1, 2)
+nodo2 = nodo.Nodo(3, 4)
+nodo3 = nodo.Nodo(5, 6)
+route=rt.Route()
+route2=rt.Route()
+route.aggiungi_nodo(nodo1)
+route.aggiungi_nodo(nodo2)
+route.aggiungi_nodo(nodo3)
+route2.aggiungi_nodo(nodo1)
+route2.aggiungi_nodo(nodo2)
+route2.aggiungi_nodo(nodo3)
+route.set_km()
+route2.set_km()
+listaaaa= [route, route2]
+print(str(ut.kmtot(listaaaa)))
+print(str(route))
 
