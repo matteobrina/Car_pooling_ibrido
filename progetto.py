d32b1b34a0cae0fd783b2f8c94e9e8b863aed00c
Matteo Brina <matteobrina99@gmail.com>
2023-08-19 16:01:47 +0200

    creata la funzione che stampa il grafico

--- progetto.py
+++ progetto.py
@@ -1,40 +1,14 @@
-from operator import attrgetter
 import nodo
 import node_generator as ng
-import cluster as cl
-import utili as ut
-import route as rt
-import fermata as fe
 import fermata_generator as fg
 import cluster_first as cf
 import cheapest_insertion as ci
 import pickle
 import matplotlib.pyplot as plt
-
-
-"""lista= ng.genera_lista()
-for el in lista:
-    print(el)
-
-unife = nodo.Nodo(321, 765)
-
-with open('nodi.txt', 'w') as f:
-    for el in lista:
-        f.write(str(el)+ '\n')
-
-listaf = fg.genera_lista()
-for el in listaf:
-    print(el)
-
-
-cluster= cf.Cluster_set(lista, unife, listaf)
-for el in cluster.get_cluster_set():
-    print(el)
-    print(ci.routing(el))"""
+import copy
 
 
 
-unife = nodo.Nodo(321, 765)
 """lista1 = ng.genera_lista()
 for el in lista1:
     print(el)
@@ -48,6 +22,9 @@ for el in lista2:
 with open('fermate.dat', 'wb') as f:
     pickle.dump(lista2, f)"""
 
+unife = nodo.Nodo(321, 765)
+
+
 with open('nodi.dat', 'rb') as f:
     lista1 = pickle.load(f)
 for el in lista1:
@@ -59,9 +36,24 @@ for el in lista2:
     print(el)
 
 cluster= cf.Cluster_set(lista1, unife, lista2)
+cluster2 = copy.deepcopy(cluster)
 for el in cluster.get_cluster_set():
     print(el)
     print(ci.routing(el))
+
+plt.figure()
+for el in cluster2.get_cluster_set():
+    route=ci.routing(el)
+    lista=route.get_nodi()
+    lista_x=[]
+    lista_y=[]
+    for el2 in lista:
+        lista_x.append(el2.get_x())
+        lista_y.append(el2.get_y())
+    plt.plot(lista_x, lista_y, color='blue', marker='o')
+
+
+plt.savefig("figura.png")
 
 
 
