5c555ff314d02efa284b3d947894b627647f7b0c
Matteo Brina <matteobrina99@gmail.com>
2023-08-20 11:43:28 +0200

    trasformata la parte di codice che crea il grafico in una funzione per riusablità

--- progetto.py
+++ progetto.py
@@ -1,11 +1,9 @@
 import nodo
-import node_generator as ng
-import fermata_generator as fg
 import cluster_first as cf
 import cheapest_insertion as ci
 import pickle
-import matplotlib.pyplot as plt
 import copy
+import grafico as gf
 
 
 
@@ -41,43 +39,9 @@ for el in cluster.get_cluster_set():
     print(el)
     print(ci.routing(el))
 
-#inizio grafico
-plt.figure(figsize=(30, 30))
-plt.grid()
-plt.xticks(range(0, 1001, 50))
-plt.yticks(range(0, 1001, 50))
+gf.grafico(lista1, lista2, cluster2, "figura.png")
 
 
-
-for el in lista1:
-    plt.plot(el.get_x(), el.get_y(), color='pink', marker='o')
-    plt.annotate(el.get_id(), (el.get_x(), el.get_y()), textcoords="offset points", xytext=(0,10), ha='center')
-
-
-
-for el in cluster2.get_cluster_set():
-    route=ci.routing(el)
-    lista=route.get_nodi()
-    lista_x=[]
-    lista_y=[]
-    lista_i=[]
-    for el2 in lista:
-        lista_x.append(el2.get_x())
-        lista_y.append(el2.get_y())
-        lista_i.append(el2.get_id())
-    plt.plot(lista_x, lista_y, color='blue', marker='o', linewidth=0.5)
-    for i, label in enumerate(lista_i):
-        plt.annotate(label, (lista_x[i], lista_y[i]), textcoords="offset points", xytext=(0,10), ha='center')
-
-for el in lista2:
-    plt.plot(el.get_x(), el.get_y(), color='orange', marker='o')
-    plt.annotate(el.get_l(), (el.get_x(), el.get_y()), textcoords="offset points", xytext=(10,0), ha='center')
-
-plt.plot(321, 765, color='red', marker='o')
-
-plt.savefig("figura.png")
-
-#fine grafico
 
 
 
